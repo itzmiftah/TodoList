ee6fe2ee23ab7689c7259a475b59b0a7b8cfaadb
Md Miftahul Islam <miftahulislam138@gmail.com>
2023-10-02 22:24:01 +0530

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,51 @@
+
+const About = () => {
+  return (
+    <>
+    
+    <div className="w-2/3 ">
+      <div className="whitespace-normal ">
+      <h1 className="font-bold m-4">  Creating a to-do list can be a helpful way to stay organized and manage your tasks effectively. Here's an example of a to-do list with various categories and tasks:
+
+Personal Tasks:</h1>
+
+ <p className="font-semi-bold m-4">Buy groceries for the week.</p> 
+ <p className="font-semi-bold m-4">Go for a 30-minute jog.</p> 
+ <p className="font-semi-bold m-4">Call mom to wish her a happy birthday.</p> 
+  <p className="font-semi-bold m-4">Schedule a dentist appointment.</p>
+  <p className="font-semi-bold m-4">Read 2 chapters of the new book.</p>
+
+  <h3 className="font-bold m-4">Work Tasks:</h3> 
+
+    <p className="font-semi-bold m-4"> Send the weekly progress report to the team.</p>
+
+  <p className="font-semi-bold m-4">Prepare for the client presentation on Friday.</p>
+  <p className="font-semi-bold m-4">Attend the marketing team meeting at 2 PM.</p>
+  <p className="font-semi-bold m-4">Complete the expense report for last month.</p>
+  <p className="font-semi-bold m-4">Respond to urgent emails.</p>
+
+
+  <h3 className="font-bold m-4">Home Improvement:</h3>
+
+  <p className="font-semi-bold m-4">Fix the leaky faucet in the bathroom.</p>
+
+   <p className="font-semi-bold m-4">Organize the garage and declutter.</p>
+   <p className="font-semi-bold m-4">Paint the living room walls.</p>
+   <p className="font-semi-bold m-4">Install new curtains in the bedroom.</p>
+   <p className="font-semi-bold m-4">Research landscaping ideas for the backyard.</p>
+</div>
+</div>
+    
+    
+    </>
+     
+  )
+}
+
+export default About
+
+export const metadata = {
+ 
+  title: 'Todo-About',
+  description: 'this is about page of todo list',
+}
